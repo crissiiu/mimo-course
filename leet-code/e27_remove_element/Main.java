1529ee4b0188ae75ecfd01c60ae650d5f8f9f78c
crissiiu <crissiiu.dev@gmail.com>
2026-03-30 21:26:45 +0700

    e27_remove_element

---
+++ leet-code/e27_remove_element/Main.java
@@ -0,0 +1,26 @@
+package e27_remove_element;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        int result = removeElement(nums, val);
+        System.out.println(result);
+
+    }
+    public static int removeElement(int[]nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while(i <= j) {
+            if(nums[i] != val) {
+                i++;
+            } else {
+                int temp =nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+        }
+        return i;
+    }
+}
