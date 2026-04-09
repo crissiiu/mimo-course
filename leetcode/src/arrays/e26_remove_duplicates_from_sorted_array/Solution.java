f5db94134dca9a5e7b71d88926a72bd2cc2d16f5
ductv24.ai <ductv24.ai@gmail.com>
2026-04-09 21:45:28 +0700

    leetcode: remove duplicate from sorted array

---
+++ leetcode/src/arrays/e26_remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,22 @@
+package arrays.e26_remove_duplicates_from_sorted_array;
+
+public class Solution {
+    void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicate(nums));
+    }
+
+    int removeDuplicate(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1)
+            return n;
+        int i = 1, j = 1;
+        while (j < n) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i++] = nums[j];
+            }
+            j++;
+        }
+        return i;
+    }
+}
