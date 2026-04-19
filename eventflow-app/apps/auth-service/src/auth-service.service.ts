d2662be8d3fc8f77b43b7576dfcfc82636c1118a
crissiiu.dev@gmail.com <Sieu@08042003>
2026-04-19 14:10:07 +0700

    Microservice backend: Done setup kafka nestjs

--- eventflow-app/apps/auth-service/src/auth-service.service.ts
+++ eventflow-app/apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
+import { KafkaService } from '@app/kafka/kafka.service';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthServiceService {
-  getHello(): string {
-    return 'Hello World!';
+export class AuthServiceService implements OnModuleInit{
+  constructor(
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+  ){}
+
+  async onModuleInit(){
+    //connect to kafka when module initializes
+    await this.kafkaClient.connect();
+  }
+
+  getHello(): string{
+    return 'Hello world';
+  }
+
+  simulateUserRegistration(email: string) {
+    //publish event to kafka
+    this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
+      email,
+      timestamp : new Date().toISOString()
+    });
+
+    return {message:`User registered: ${email}`}
   }
 }
