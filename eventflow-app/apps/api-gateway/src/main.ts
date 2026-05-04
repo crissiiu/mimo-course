925151e0cc542bc2247ea0e4d87e5e6e4cc3bb4e
crissiiu.dev@gmail.com <Sieu@08042003>
2026-05-04 23:58:21 +0700

    Microservice: api-gateway

--- eventflow-app/apps/api-gateway/src/main.ts
+++ eventflow-app/apps/api-gateway/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SERVICE_PORT } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? SERVICE_PORT.API_GATEWAY);
+
+  // Enable Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(SERVICE_PORT.API_GATEWAY);
 }
 bootstrap();
